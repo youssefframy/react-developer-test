6149387aba297302a86cd4764b95adef34af0f66
Youssef <youssefframy>
2022-06-24 18:14:16 +0200

    finished out of stock product styling

--- src/components/Category-item.jsx
+++ src/components/Category-item.jsx
@@ -18,17 +18,36 @@ const ProductContainer = styled.div`
     box-shadow: 0px 4px 35px rgba(168, 172, 176, 0.19);
   }
 `
-const ImageContainer = styled.div`
+const InStock = styled.div`
   width: 356px;
   height: 300px;
   overflow: hidden;
 `
+const OutStock = styled.div`
+  text-align: center;
+  justify-content: center;
+
+  p{
+    position: absolute;
+    color: #8D8F9A;
+    left: 25.42%;
+    right: 25.71%;
+    top: 30%;
+    bottom: 43.94%;
+    font-weight: 400;
+    font-size: 1.5rem;
+  }
+
+  img{
+    opacity: 0.5;
+  }
+`
+
 const Image = styled.img`
   width: 356px;
   height: 300px;
   justify-content: center;
   object-fit: cover;
-
 `
 
 const DescriptionContainer= styled.div`
@@ -50,12 +69,13 @@ class CategoryItem extends Component {
   render() {
     const product = this.props.product;
     const currency = this.props.selectedCurrency;
+
     return (
       <ProductContainer>
-      <ImageContainer>
-        <Image src={product.gallery[0]} alt={product.name}/>
-      </ImageContainer>
-
+      { product.inStock 
+        ? <InStock> <Image src={product.gallery[0]} alt={product.name}/> </InStock>
+        : <OutStock> <p>OUT OF STOCK</p><Image src={product.gallery[0]} alt={product.name}/> </OutStock>
+      }
       <DescriptionContainer>
         <Description>{product.name}</Description>
         <Description>{`${currency[0].currency.symbol} ${currency[0].amount}`}</Description>
